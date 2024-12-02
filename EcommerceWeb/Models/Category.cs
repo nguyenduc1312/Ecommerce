28282341b2304d14bda814eefe45ec2250991c33
duc_99 <nguyenduc1312.pt@gmail.com>
2024-12-02 23:14:29 +0700

    second commit

---
+++ EcommerceWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Category Name")]
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
